2b603f9003030af2a1ba49bcd40bd130835e14d5
v0l0 <podolsky35alex@gmail.com>
2026-06-16 09:36:07 +0300

    fix map

--- steam/steam.js
+++ steam/steam.js
@@ -1,4 +1,4 @@
-const STEAM_KEY = process.env.STEAM_API_KEY || "STEAM_API_KEY_NOT_SET"
+const STEAM_KEY = process.env.STEAM_API_KEY || "STEAM_API"
 
 async function getAvatar(steamid) {
   if (!steamid) return null
